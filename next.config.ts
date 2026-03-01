e5fea6dd70b44855d081484fd950407d47cb81df
FantomStudy <arzamastsevdaniil@gmail.com>
2026-03-01 19:57:25 +0500

    feat: enable photo optimization and remove ide folder

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,21 @@
 import type { NextConfig } from "next";
 
+const S3_HOSTNAME = process.env.S3_HOSTNAME;
+if (!S3_HOSTNAME) {
+  throw new Error("S3_HOSTNAME is not defined in .env");
+}
+
 const nextConfig: NextConfig = {
+  typedRoutes: true,
   reactStrictMode: true,
   output: "standalone",
-  typedRoutes: true,
 
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
+        protocol: "https",
+        hostname: S3_HOSTNAME,
+        port: "",
         pathname: "/**",
       },
     ],
