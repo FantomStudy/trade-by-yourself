2dea09769cf632579b5c51b9dfd632069bd1e0b4
FantomStudy <arzamastsevdaniil@gmail.com>
2026-05-11 23:38:02 +0500

    refactor: remove unused functions

--- src/lib/market/users.ts
+++
@@ -1,6 +0,0 @@
-import { apiPut } from "./api";
-
-export const setUserRole = (
-  userId: number,
-  role: "USER" | "USER_VERIFIED" | "SENIOR_MODERATOR" | "ADMIN" | "SUPERADMIN",
-) => apiPut(`/user/${userId}/role`, { role });
